c1c5f1a0fe6215b37a0bcde443fb0274c2b13c55
Zeynep Kazu Girtine <zeynepkazugirtine@Zeyneps-MacBook-Pro.local>
2024-01-08 17:16:30 +0300

    minor fix and added comments

--- repeating_characters/src/main/java/org/example/Main.java
+++ repeating_characters/src/main/java/org/example/Main.java
@@ -4,16 +4,21 @@ import java.util.*;
 
 public class Main {
 
+    //initialize an input string with words, calls the shuffleCharactersAndCombineWords method, and prints the result.
     public static void main(String[] args) {
         String inputString = "This is an algorithm to make a new word with words that has repeated characters";
         String result = shuffleCharactersAndCombineWords(inputString);
         System.out.println("Randomized characters word: " + result);
     }
 
+    //takes an input string, splits it into an array of words using spaces as separators,
+    // and initializes an empty list to store words with repeated characters.
     public static String shuffleCharactersAndCombineWords(String inputString) {
         List<String> wordsWithRepeatedCharacters = new ArrayList<>();
         String[] words = inputString.split("\\s+");
 
+        //loop iterates over each word in the array, and for words with repeated characters
+        // it converts the word into a char array and then into a list of characters.
         for (String word : words) {
             if (hasRepeatedCharacters(word)) {
                 char[] charArray = word.toCharArray();
@@ -33,6 +38,7 @@ public class Main {
         return String.join("", wordsWithRepeatedCharacters);
     }
 
+    //method returns a single string by joining the shuffled words in the list
     public static boolean hasRepeatedCharacters(String word) {
         Set<Character> uniqueCharacters = new HashSet<>();
 
