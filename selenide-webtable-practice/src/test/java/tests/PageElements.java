097df33550ea153c110593db51d3c089e139ef98
Zeynep Kazu Girtine <zeynepkazu@gmail.com>
2024-03-13 17:06:29 +0300

    separated elements

---
+++ selenide-webtable-practice/src/test/java/tests/PageElements.java
@@ -0,0 +1,27 @@
+package tests;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PageElements {
+
+    //Form fields
+    protected SelenideElement addNewRecordButton = $("#addNewRecordButton");
+    protected SelenideElement registrationForm = $("#registration-form-modal");
+    //Declaring form fields for better readability
+    protected SelenideElement firstNameField = $("#firstName");
+    protected SelenideElement lastNameField = $("#lastName");
+    protected SelenideElement emailField = $("#userEmail");
+    protected SelenideElement ageField = $("#age");
+    protected SelenideElement salaryField = $("#salary");
+    protected SelenideElement departmentField = $("#department");
+
+    //Buttons
+    protected SelenideElement submitButton =  $("#submit");
+    protected SelenideElement editButton = $("#edit-record-4");
+
+    }
+
--- selenide-webtable-practice/src/test/java/tests/SelenideWebTableTest.java
+++ selenide-webtable-practice/src/test/java/tests/SelenideWebTableTest.java
@@ -7,38 +7,28 @@ import org.testng.annotations.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static com.codeborne.selenide.Selenide.*;
 
-public class SelenideWebTableTest {
-
+public class SelenideWebTableTest extends PageElements {
     //added timeout so that the tests will not fail due to a timeout error
     @BeforeTest
     public void setUp() {
-        Configuration.timeout = 45000;
+        Configuration.timeout = 50000;
     }
 
     @Test
-    public void openWebTables() {
+    public void navigateAndAssert () {
         open("https://demoqa.com/webtables");
         $("#addNewRecordButton").should(Condition.exist);
         assertThat("https://demoqa.com/webtables");
     }
 
+
     @Test
     public void submitAndEditNewEntry() {
         open("https://demoqa.com/webtables");
 
-        SelenideElement addNewRecordButton = $("#addNewRecordButton");
         addNewRecordButton.scrollTo().click();
 
-        SelenideElement registrationForm = $("#registration-form-modal");
         registrationForm.shouldBe(Condition.visible);
-
-        //Declaring form fields for better readability
-        SelenideElement firstNameField = $("#firstName");
-        SelenideElement lastNameField = $("#lastName");
-        SelenideElement emailField = $("#userEmail");
-        SelenideElement ageField = $("#age");
-        SelenideElement salaryField = $("#salary");
-        SelenideElement departmentField = $("#department");
 
         //Setting form fields
         firstNameField.setValue("James");
@@ -48,10 +38,8 @@ public class SelenideWebTableTest {
         salaryField.setValue("50000");
         departmentField.setValue("IT");
 
-        SelenideElement submitButton =  $("#submit");
         submitButton.click();
 
-        SelenideElement editButton = $("#edit-record-4");
         editButton.click();
 
         //Editing the new record
